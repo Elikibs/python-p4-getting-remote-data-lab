a832853186889294fcf6e1215014b7e339ab2379
Elisha <elishakibet67@gmail.com>
2024-01-11 14:06:53 +0300

    Complete assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,26 @@
 import requests
 import json
 
+# General class for getting nd working with API data
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        workers_list = []
+        workers = json.loads(self.get_response_body())
+        
+        for worker in workers:
+            workers_list.append(f'Name; {worker["name"]} Occupation; {worker["occupation"]}')
+        
+        return workers_list
+
+get_requester = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+workers_list_output = get_requester.load_json()
+
+for worker in workers_list_output:
+    print(worker)
